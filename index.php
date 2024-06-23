bb09b684c019cdf9abcc141cf178e36290c747f8
Pasan3885 <pasan99piyumal@gmail.com>
2024-06-23 13:51:09 +0530

    add login icon and username feild
    add login icon to the home page.
    add user field to the login.php.
    psw renamed as password
    psw-repeat renamed as re-password
    finally added a java script to get url parameter and display the msg

--- index.php
+++ index.php
@@ -22,6 +22,11 @@
 		<li id="lg-bag"><a href="cart.php"><i class="fa fa-shopping-bag"></i></a></li>
 		<li id="lg-bag"><a href="login.php"><i class="fa fa-user-plus"></i></a></li>
 		<a href="#" id="close"><i class="fa fa-times"></i></a>
+		<!--adding logout icon-->
+		<li id="user-info">
+			<a href="#"><i class="fa fa-user"></i></a>
+			<a href="logout.php"><i class="fa fa-sign-out"></i> Logout</a>
+		</li>
 		
 	 </ul>
 	</div>
--- login.php
+++ login.php
@@ -5,23 +5,26 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title>E- CommerceWebsite</title>
         <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css"/>
-        <link rel="stylesheet" type="text/css" href="stylelogin.css">
+        <link rel="stylesheet" type="text/css" href="css/stylelogin.css">
      </head>
      <body>
-        <form action="action_page.php" style="border:1px solid #ccc">
+        <form action="register.php" style="border:1px solid #ccc">
             <div class="container">
               <h1>Sign Up</h1>
               <p>Please fill in this form to create an account.</p>
               <hr>
+              <!--add username column-->
+              <label for="username"><b>Username</b></label>
+              <input type="text" placeholder="Enter Your Username" name="username" required>
           
               <label for="email"><b>Email</b></label>
               <input type="text" placeholder="Enter Email" name="email" required>
           
               <label for="psw"><b>Password</b></label>
-              <input type="password" placeholder="Enter Password" name="psw" required>
+              <input type="password" placeholder="Enter Password" name="password" required>
           
               <label for="psw-repeat"><b>Repeat Password</b></label>
-              <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
+              <input type="password" placeholder="Repeat Password" name="re_password" required>
           
               <label>
                 <input type="checkbox" checked="checked" name="remember" style="margin-bottom:15px"> Remember me
@@ -34,7 +37,28 @@
                 <button type="button" class="cancelbtn">Cancel</button>
                 <button type="submit" class="signupbtn">Sign Up</button>
               </div>
+              <!--add message coantainer to display the error/msg when register to the system-->
+              <div id="messageContainer"></div>
             </div>
-          </form> 
+          </form>
+
+          <script>
+            // Function to get URL parameter
+            function getUrlParameter(name) {
+                name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+                var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+                var results = regex.exec(location.search);
+                return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+            }
+    
+            // Display the message if exists
+            window.onload = function() {
+                var message = getUrlParameter('message');
+                if (message) {
+                    var messageContainer = document.getElementById('messageContainer');
+                    messageContainer.innerHTML = '<div class="' + (message.includes('successfully') ? 'message' : 'error') + '">' + message + '</div>';
+                }
+            }
+        </script>
      </body>
 </html>
