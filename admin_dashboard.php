977cde20188f5128a4e9ed82b80ad73de1a480a3
DilanPadukka <iit21038@std.uwu.ac.lk>
2024-08-04 16:15:43 +0530

    Update admin dashboard

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -190,11 +190,33 @@ if (!isset($_SESSION['is_admin']) || $_SESSION['is_admin'] !== true) {
                     <th>ID</th>
                     <th>Date</th>
                     <th>Price</th>
-                    <th>Quantity</th>
+                    <th>Status</th>
                 </tr>
             </thead>
             <tbody>
-                <!-- Order data -->
+            <?php
+                require_once './DbConnector.php';
+                $dbconnector = new DbConnector();
+                $con = $dbconnector->getConnection();
+
+                // read all rows from product table
+                $sql = "SELECT * FROM orders";
+                $result = $con->query($sql);
+
+                if (!$result) {
+                    die("Invalid query: " . $con->errorInfo());
+                }
+                // table data
+                while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+                    echo "<tr>
+                        <td>" . $row["customer_id"] . "</td>
+                        <td>" . $row["order_date"] . "</td>
+                        <td>" . $row["total_amount"] . "</td>
+                        <td>" . $row["status"] . "</td>
+                        </tr>";
+                }
+
+                ?>
             </tbody>
         </table>
 
